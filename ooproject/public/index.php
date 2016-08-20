7d4b974b802b20b30aa15a751cece5706473d399
phpcrazy <phpcrazy@gmail.com>
2016-08-20 12:54:50 +0630

    Change DB

--- ooproject/public/index.php
+++ ooproject/public/index.php
@@ -1,9 +1,21 @@
+
 <?php
 
+
+$stus = DB::table("students")->where('name')->get();
 $students = DB::table("students")->get();
-$stus = DB::table("students")->where('name', 'address')->get();
-var_dump($students);
-var_dump($stus);
+$classes = DB::table("classes")->get();
+$student1 = [
+    'name'  => 'Mya Mya'
+];
+DB::table("students")->insert($student1);
+$student2 = [
+    'name'  => 'Hla Hla'
+];
+
+DB::table("students")->delete(1);
+$id = DB::table('students')->insertWithId($student);
+
 
 class DB extends PDO {
 
@@ -24,23 +36,49 @@ class DB extends PDO {
 
     private $table_name;
 
+    private $select = "SELECT ";
+
+    private $where;
+
     public static function table($table_name) {
         if(!self::$_instance instanceof DB) {
             self::$_instance = new DB();
         }
         self::$_instance->table_name = $table_name;
+        self::$_instance->where = false;
         return self::$_instance;
     }
 
+    public function insert() {
+        $sql = "INSERT INTO " . $table_name . "(";
+        $keys = array_keys($fields);
+        $values = array_values($fields);
+        $i_keys = implode(",", $keys);
+        $i_values = implode('","', $values);
+        $sql .= $i_keys . ') VALUES ("' . $i_values . '")';
+
+
+    }
+
     public function get() {
-        $prep = $this->prepare("SELECT * FROM " . $this->table_name);
+        if($this->where == true) {
+            $prep = $this->prepare($this->select . " FROM " . $this->table_name);    
+        } else {
+            $prep = $this->prepare("SELECT * FROM " .$this->table_name);
+            
+        }
+        
         $prep->execute();
         return $prep->fetchAll(PDO::FETCH_ASSOC);
     }
 
     // Scalar type declarations
     public function where(string ...$values) {
-        var_dump($values);
+        $this->where = true;
+        $selected_value = implode(", ", $values);
+        $this->select .= $selected_value;
+    
+        return $this;
     }
 
     public function __construct(){
